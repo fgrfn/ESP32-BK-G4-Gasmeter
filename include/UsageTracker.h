89926d91fcabf8ea5e4e63135ad8ea2d74561377
Florian <80332665+fgrfn@users.noreply.github.com>
2026-07-15 00:52:20 +0200

    feat: add time-safe usage history and flow alerts

--- include/UsageTracker.h
+++ include/UsageTracker.h
@@ -11,34 +11,51 @@ struct UsageSnapshot {
   float dayM3 = 0.0f;
   float monthM3 = 0.0f;
   float yearM3 = 0.0f;
-  float dayCost = 0.0f;
-  float monthCost = 0.0f;
-  float yearCost = 0.0f;
+  float dayEnergyKwh = 0.0f;
+  float monthEnergyKwh = 0.0f;
+  float yearEnergyKwh = 0.0f;
+  uint32_t continuousFlowMinutes = 0;
+  bool continuousFlowAlert = false;
+  bool timeSynchronized = false;
   time_t timestamp = 0;
+};
+
+struct DailyUsageRecord {
+  char date[11] = "";
+  float volumeM3 = 0.0f;
+  float energyKwh = 0.0f;
 };
 
 class UsageTracker {
  public:
   static void begin();
-  static void update(float rawVolumeM3, time_t now);
+  static void update(float rawVolumeM3, time_t now, bool timeSynchronized);
   static const UsageSnapshot& snapshot();
+  static const DailyUsageRecord* dailyHistory(size_t& count);
 
  private:
-  static void loadBaselines();
-  static void saveBaselines();
-  static void updatePeriods(float volume, time_t now);
+  static void loadState();
+  static void saveState();
+  static void loadHistory();
+  static void saveHistory();
+  static void addDailyRecord(const char* date, float volumeM3, float energyKwh);
+  static void updatePeriods(float volume, float energy, time_t now);
+  static void updateContinuousFlow(float flowM3h);
   static void keyForTime(time_t now, char* dayKey, size_t daySize, char* monthKey, size_t monthSize, char* yearKey, size_t yearSize);
   static Preferences preferences_;
   static UsageSnapshot snapshot_;
   static float previousVolume_;
-  static time_t previousTimestamp_;
+  static uint32_t previousReadingMs_;
   static float dayBaseline_;
   static float monthBaseline_;
   static float yearBaseline_;
+  static float dayEnergyBaseline_;
+  static float monthEnergyBaseline_;
+  static float yearEnergyBaseline_;
   static float cumulativeEnergyKwh_;
-  static float dayVariableCost_;
-  static float monthVariableCost_;
-  static float yearVariableCost_;
+  static uint32_t continuousFlowStartedMs_;
+  static DailyUsageRecord dailyHistory_[31];
+  static size_t dailyHistoryCount_;
   static char dayKey_[11];
   static char monthKey_[8];
   static char yearKey_[5];
