02a319ab89a44ecc9215ee972b6e7076e688d011
Florian <80332665+fgrfn@users.noreply.github.com>
2026-07-15 01:04:12 +0200

    fix: preserve single meter increments

--- lib/CoreLogic/src/CoreLogic.h
+++ lib/CoreLogic/src/CoreLogic.h
@@ -6,7 +6,7 @@
 namespace CoreLogic {
 uint32_t clampPollIntervalMs(uint32_t value);
 float calculateFlowM3h(float previousVolume, float currentVolume, uint32_t elapsedSeconds, float maxFlowM3h);
-float positiveDelta(float previousVolume, float currentVolume, float tolerance = 0.001f);
+float positiveDelta(float previousVolume, float currentVolume, float tolerance = 0.00001f);
 bool isValidHostname(const char* value);
 bool isValidIsoDate(const char* value);
 bool isValidMqttBaseTopic(const char* value);
