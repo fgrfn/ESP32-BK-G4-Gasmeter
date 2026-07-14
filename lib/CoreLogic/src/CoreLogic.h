38867972ffd9671476cbe0f0da361abf6d7cac09
Florian <80332665+fgrfn@users.noreply.github.com>
2026-07-14 16:50:25 +0200

    fix: validate configuration dates safely

--- lib/CoreLogic/src/CoreLogic.h
+++ lib/CoreLogic/src/CoreLogic.h
@@ -7,5 +7,6 @@ namespace CoreLogic {
 uint32_t clampPollIntervalMs(uint32_t value);
 float calculateFlowM3h(float previousVolume, float currentVolume, uint32_t elapsedSeconds, float maxFlowM3h);
 bool isValidHostname(const char* value);
+bool isValidIsoDate(const char* value);
 void makeSafeId(const char* input, char* output, size_t outputSize);
 }
