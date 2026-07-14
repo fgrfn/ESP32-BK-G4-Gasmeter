3dcb72626246d32342bbcfde656b2c2ec2e44579
Florian <80332665+fgrfn@users.noreply.github.com>
2026-07-15 00:48:53 +0200

    feat: add runtime validation helpers

--- lib/CoreLogic/src/CoreLogic.h
+++ lib/CoreLogic/src/CoreLogic.h
@@ -6,7 +6,10 @@
 namespace CoreLogic {
 uint32_t clampPollIntervalMs(uint32_t value);
 float calculateFlowM3h(float previousVolume, float currentVolume, uint32_t elapsedSeconds, float maxFlowM3h);
+float positiveDelta(float previousVolume, float currentVolume, float tolerance = 0.001f);
 bool isValidHostname(const char* value);
 bool isValidIsoDate(const char* value);
+bool isValidMqttBaseTopic(const char* value);
+bool isSynchronizedEpoch(int64_t epochSeconds);
 void makeSafeId(const char* input, char* output, size_t outputSize);
 }
